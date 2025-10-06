8f30427bd2ee9e5925b6b5fff1254cc2fd0e66d2
UdaraDewshan <dewudara@gmail.com>
2025-10-06 22:54:52 +0530

    Add singUp Page Component

--- src/main/java/controller/SingUp_Form_Controller.java
+++ src/main/java/controller/SingUp_Form_Controller.java
@@ -1,4 +1,65 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SingUp_Form_Controller {
+
+    @FXML
+    private Label accountNameLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label nicLabel;
+
+    @FXML
+    private Label passLabel;
+
+    @FXML
+    private Hyperlink signInLink;
+
+    @FXML
+    private Button signUpBtn;
+
+    @FXML
+    private Hyperlink termsLink;
+
+    @FXML
+    private TextField txtAName;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFName;
+
+    @FXML
+    private TextField txtLName;
+
+    @FXML
+    private TextField txtNIC;
+
+    @FXML
+    private PasswordField txtPass;
+
+    @FXML
+    private PasswordField txtPassCon;
+
+    @FXML
+    void btnSingUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInAction(ActionEvent event) {
+
+    }
+
 }
