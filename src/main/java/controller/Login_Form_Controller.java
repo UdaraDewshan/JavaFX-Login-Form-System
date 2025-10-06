85dcedba0a1da47e755066d45f376945aea9f0b3
UdaraDewshan <dewudara@gmail.com>
2025-10-06 16:16:35 +0530

    Add SingUp Fxml

--- src/main/java/controller/Login_Form_Controller.java
+++ src/main/java/controller/Login_Form_Controller.java
@@ -1,15 +1,52 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Login_Form_Controller {
-    
-    
-    public void forgotPassLinkAction(ActionEvent actionEvent) {
-        
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Hyperlink forgotPassLink;
+
+    @FXML
+    private Hyperlink signUpLink;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void forgotPassLinkAction(ActionEvent event) {
+
     }
 
 
-    public void signUpLinkAction(ActionEvent actionEvent) {
+    @FXML
+    void signUpLinkAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/SingUp_Form.fxml"))));
+        Stage stage1 = (Stage) signUpLink.getScene().getWindow();
+        stage1.close();
+        stage.show();
     }
+
+    @FXML
+    void btnSinginAction(ActionEvent event) {
+
+    }
+
 }
