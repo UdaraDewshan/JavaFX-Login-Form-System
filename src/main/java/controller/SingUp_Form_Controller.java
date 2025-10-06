00a61d4ef4c4f9144d7d162a748722df9c567455
UdaraDewshan <dewudara@gmail.com>
2025-10-06 23:23:27 +0530

    Add Action Singup Page To go SingIn page

--- src/main/java/controller/SingUp_Form_Controller.java
+++ src/main/java/controller/SingUp_Form_Controller.java
@@ -2,11 +2,16 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SingUp_Form_Controller {
 
@@ -59,7 +64,15 @@ public class SingUp_Form_Controller {
 
     @FXML
     void signInAction(ActionEvent event) {
-
+        Stage stage =new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login_Form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage1 = (Stage) signInLink.getScene().getWindow();
+        stage1.close();
+        stage.show();
     }
 
 }
